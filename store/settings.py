9cf0528f3ac65cf0bd4835139e1fd5c5bfafd019
ms20002000 <shojaeim2000@gmail.com>
2024-12-04 07:56:25 +0330

    database

--- store/settings.py
+++ store/settings.py
@@ -87,6 +87,17 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+# 'default': {
+#     'ENGINE': 'django.db.backends.postgresql',
+#     'NAME': 'store',
+#     'USER': 'mohammad',
+#     'PASSWORD': '',
+#     'HOST': 'localhost', 
+#     'PORT': '5432', 
+#     }
+# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
