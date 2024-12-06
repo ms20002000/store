7b88a65530f798ee04c205cc1699534f5c8eb396
ms20002000 <shojaeim2000@gmail.com>
2024-12-06 13:48:12 +0330

    config postgres database

--- discount/models.py
+++ discount/models.py
@@ -5,6 +5,7 @@ class Discount(BaseModel):
     discount = models.IntegerField(null=True, blank=True)
     expire_at = models.DateTimeField()
     max_amount = models.PositiveIntegerField(null=True, blank=True)
+    discount_type = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return str(self.discount)
